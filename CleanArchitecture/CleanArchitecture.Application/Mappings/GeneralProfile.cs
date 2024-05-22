c78d03dc57696e6d74b685e012a22e7be0ab70b1
onur alp akin <20230808608@ogr.akdeniz.edu.tr>
2024-05-22 01:50:12 -0700

    Add project files.

---
+++ CleanArchitecture/CleanArchitecture.Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Features.Products.Commands.CreateProduct;
+using CleanArchitecture.Core.Features.Products.Queries.GetAllProducts;
+
+namespace CleanArchitecture.Core.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<Product, GetAllProductsViewModel>().ReverseMap();
+            CreateMap<CreateProductCommand, Product>();
+            CreateMap<GetAllProductsQuery, GetAllProductsParameter>();
+        }
+    }
+}
